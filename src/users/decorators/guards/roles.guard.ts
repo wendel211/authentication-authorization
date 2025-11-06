f4db87605a8e6be1003174b134501654790c3e99
wendel211 <wendelmuniz04@gmail.com>
2025-11-05 23:49:27 -0300

    Fix: Correct import paths in JwtAuthGuard and RolesGuard

--- src/users/decorators/guards/roles.guard.ts
+++ src/users/decorators/guards/roles.guard.ts
@@ -1,8 +1,8 @@
 
 import { CanActivate, ExecutionContext, Injectable, ForbiddenException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { ROLES_KEY } from '../decorators/roles.decorator';
-import { UserRole } from '../../users/user.entity';
+import { ROLES_KEY } from '../../decorators/roles.decorator';
+import { UserRole } from '../../../users/user.entity';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
