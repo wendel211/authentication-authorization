2cd7356925a4d8990eb8fd93938020da129aaeba
wendel211 <wendelmuniz04@gmail.com>
2025-11-05 23:06:39 -0300

    feat: Add authentication DTOs and JWT strategy with payload validation.

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtPayload } from '../types/jwt-payload.type';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get<string>('JWT_ACCESS_SECRET'),
+    });
+  }
+
+  validate(payload: JwtPayload) {
+    // vira req.user
+    return payload;
+  }
+}
---
+++ src/auth/types/jwt-payload.type.ts
@@ -0,0 +1,5 @@
+export type JwtPayload = {
+  sub: string;  // id do usuário
+  email: string;
+  role: string;
+};
