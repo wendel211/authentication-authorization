5f8d95df89aca691294fac3bee888cbaf65f14ec
wendel211 <wendelmuniz04@gmail.com>
2025-11-05 23:16:58 -0300

    feat: Configure Swagger and cookie-parser in the application's bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('Projeto 3 - Auth JWT')
+    .setDescription('Login, JWT, rotas protegidas, roles e refresh token.')
+    .setVersion('1.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'access-token',
+    )
+    .build();
+
+  const doc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, doc);
+
+  await app.listen(3000);
 }
 bootstrap();
