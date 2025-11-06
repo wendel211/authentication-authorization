679cb36a60bd918b4436a8438b6973721d5b60eb
wendel211 <wendelmuniz04@gmail.com>
2025-11-05 22:59:03 -0300

    feat: Add GetUser and Public decorators, and implement UsersController with user retrieval and admin metrics endpoints

--- src/common/decorators/get-user.decorator.ts
+++ src/common/decorators/get-user.decorator.ts
@@ -1,1 +1,8 @@
 
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const GetUser = createParamDecorator((data: keyof any, ctx: ExecutionContext) => {
+  const req = ctx.switchToHttp().getRequest();
+  const user = req.user;
+  return data ? user?.[data] : user;
+});
---
+++ src/users/users.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+import { GetUser } from '../common/decorators/get-user.decorator';
+import { Roles } from '../common/decorators/roles.decorator';
+import { UserRole } from './user.entity';
+
+@Controller('users')
+export class UsersController {
+  @Get('me')
+  getMe(@GetUser() user: any) {
+    return { user };
+  }
+
+  @Roles(UserRole.ADMIN)
+  @Get('admin/metrics')
+  adminMetrics() {
+    return { message: 'Apenas ADMIN vê isso.' };
+  }
+}
